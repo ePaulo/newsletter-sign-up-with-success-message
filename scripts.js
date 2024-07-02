7cfd5ff69d3ad6ef7046bb25d8d1b60db907f8e5
ePaulo <paul.olynek@live.ca>
2024-07-01 23:05:26 -0700

    started working on email input error response

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Coded by Paul O
+// Date: 2024-7-1
+
+// DOM Element References
+const form = document.getElementById('form')
+const error = document.getElementById('error')
+const email = document.getElementById('email')
+
+// Event Listeners
+form.addEventListener('submit', e => {
+  e.preventDefault()
+})
